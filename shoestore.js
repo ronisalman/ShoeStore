ec80eb4550cb692691b8622b673d72c029be2d5e
Roni <ronisalman@email.arizona.edu>
2020-05-24 22:56:00 -0700

    starting code for cart movement

--- shoestore.js
+++ shoestore.js
@@ -1,2 +1,19 @@
 // Created by Roni Salman
 
+
+class cart extends React.Component{
+    message=event=>{
+        console.log("hi");
+    }
+
+    render=()=>{
+        return <button onClick={this.message}>cart</button>;
+    }
+}
+
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+  );
+  
+React.render(<cart />,document.getElementsByClassName('cartText'));
