cc9afc5d3f6375906e0d487226083e6d18108942
Roni <ronisalman@email.arizona.edu>
2020-05-25 21:01:03 -0700

    setting up react

--- shoestore.js
+++ shoestore.js
@@ -1,19 +1,28 @@
 // Created by Roni Salman
 
 
+'use strict';
+  
+const ele=React.createElement;
+
 class cart extends React.Component{
-    message=event=>{
-        console.log("hi");
+    constructor(props){
+        super(props);
+        this.state={clicked:false};
     }
 
-    render=()=>{
-        return <button onClick={this.message}>cart</button>;
+    render(){
+        if(this.state.clicked){
+            console.log("Clicked!");
+        }
+
+        return ele(
+            'button',
+            {onClick: () => this.setState({clicked: true}) },
+            'Click'
+        );
     }
 }
 
-ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('root')
-  );
-  
-React.render(<cart />,document.getElementsByClassName('cartText'));
+const domContainer=document.querySelectorAll('.btn.btn-info');
+ReactDOM.render(ele(cart),domContainer);
