42c28f05f8d89a4420c9d4e716b9b771922b59fe
Roni <ronisalman@email.arizona.edu>
2020-06-09 21:24:05 -0700

    test react code

--- shoestore.js
+++ shoestore.js
@@ -1,9 +1,16 @@
 // Created by Roni Salman
 
 const cartButton=document.querySelector('#cartTest');
+const e=React.createElement;
 
 
 const clickedCart=()=>{
     console.log('hi');
 }
 
+
+
+ReactDOM.render(
+    e('div',null,'Hello!'),
+    document.getElementById('root')
+);
