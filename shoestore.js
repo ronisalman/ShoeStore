a869ba8859e72f92ff8ae41ca39cf287756115d2
Roni <ronisalman@email.arizona.edu>
2020-07-15 21:05:51 -0700

    added onclick event to cart

--- shoestore.js
+++ shoestore.js
@@ -1,28 +1,7 @@
 // Created by Roni Salman
 
-class OpenCart extends React.Component {
-  render() {
-    return /*#__PURE__*/React.createElement("p", null, "Hello world!");
-  }
+let cartButton=document.getElementById('slidingCart');
 
+let clickedCart=()=>{
+  cartButton.classList.toggle('openCart');
 }
-
-clickedCart=()=>{
-  ReactDOM.render( /*#__PURE__*/React.createElement(OpenCart, null), document.getElementById('slidingCart'));
-}
-
-// Before compiling:
-// class OpenCart extends React.Component{
-//     render(){
-//       return(
-//         <p>Hello world!</p>
-//       )
-//     } 
-// }
-
-
-
-// ReactDOM.render(
-//     <OpenCart />,
-//     document.getElementById('slidingCart')
-// );
