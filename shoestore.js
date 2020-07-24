389f47e5109b3229b17289f4ca530599828c1237
Roni <ronisalman@email.arizona.edu>
2020-07-23 23:25:21 -0700

    clicking cart shows price text

--- shoestore.js
+++ shoestore.js
@@ -1,7 +1,19 @@
 // Created by Roni Salman
 
 let cartButton=document.getElementById('slidingCart');
+let totalText=document.getElementById('total');
+
+let activeButton=false;
 
 let clickedCart=()=>{
+  // show price when cart open, hide when cart closed
+  if(!activeButton){
+    activeButton=true;
+    totalText.innerHTML="Price: ";
+  }
+  else{
+    activeButton=false;
+    totalText.innerHTML="";
+  }
   cartButton.classList.toggle('openCart');
 }
